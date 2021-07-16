7e72000db71edb8887a17ef67014fbd1a6a05706
ravat.s <ravat.s@gurukul.org>
2021-07-16 15:26:45 +0530

    connection class changes

--- admin/class/Connection.php
+++ admin/class/Connection.php
@@ -5,7 +5,10 @@ class ConnectionClass
         
         function getConnection()
         {
-            return $connection = mysqli_connect("127.0.0.1","root","","touch_and_feel");
+             // For Local Development
+            //return $connection = mysqli_connect("127.0.0.1","root","","touch_and_feel");
+            // For Production
+            return $connection = mysqli_connect("localhost:3306","touchand_testuse","uVVHN62zVDmS5C7","touchand_test");
         }
     }
 ?>
