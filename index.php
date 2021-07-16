7e73716431823ba61a1e61f68e7f8a8182183355
ravat.s <ravat.s@gurukul.org>
2021-07-16 10:32:53 +0530

    icon image added

--- index.php
+++ index.php
@@ -584,7 +584,6 @@
           ?>
             <div class="col-lg-4 col-md-6 portfolio-item filter-app">
               <div class="portfolio-wrap">
-                <!-- <img src="assets/img/portfolio/portfolio-1.jpg" class="img-fluid" alt=""> -->
                 <img src="<?php echo substr($product_arr['image'], 3); ?>" style="width:420px !important;height:320px !important;" class="img-fluid" alt="">
                 <div class="portfolio-info">
                   <h4><?php echo $product_arr['name']; ?></h4>
@@ -599,21 +598,6 @@
           <?php
           }
           ?>
-
-
-          <!-- <div class="col-lg-4 col-md-6 portfolio-item filter-web">
-            <div class="portfolio-wrap">
-              <img src="assets/img/portfolio/portfolio-2.jpg" class="img-fluid" alt="">
-              <div class="portfolio-info">
-                <h4>Web 3</h4>
-                <p>Web</p>
-                <div class="portfolio-links">
-                  <a href="assets/img/portfolio/portfolio-2.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 3"><i class="bx bx-plus"></i></a>
-                  <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
-                </div>
-              </div>
-            </div>
-          </div> -->
 
         </div>
 
